73e5e89c3961614d47702133c2f6132fbd2af1ca
zijian.cheng <zijian.cheng@zijiancheng.local>
2018-05-04 17:10:33 +0800

    [fixed] 修改提交请求相关，适配各个项目的编译。

--- BuildProjects.py
+++ BuildProjects.py
@@ -1,4 +1,4 @@
-# coding=utf-8
+# -*- coding: UTF-8 -*-
 import os
 import sys
 import time
@@ -7,13 +7,13 @@ import jenkins
 import requests
 import xlrd
 
-from config import TARGET_HOST, PROJECT_NAME
 from lxml import etree
 
+# Jenkins 服务地址
+TARGET_HOST = "http://192.168.31.95:8080/"
 
-def startBuildProjects():
-    crawl = BuildProjects()
-    crawl.run()
+# Develop
+PROJECT_NAMES = ["MeMe-Android-Develop", "MeMe-Android-Release", "MeMe-Android-Tinker"]
 
 
 class BuildProjects(object):
@@ -21,6 +21,7 @@ class BuildProjects(object):
         self.server = jenkins.Jenkins(TARGET_HOST, timeout=5)
         self.log_list = []
         self.lines = []
+        self.select = int(sys.argv[1])
 
     def waitfor(self, getter, interval=0.5, *args):
         while True:
@@ -32,9 +33,8 @@ class BuildProjects(object):
     '''
         获取指定任务的执行进度
     '''
-
     def conCurrentProgress(self, number):
-        url = '%sjob/%s/buildHistory/ajax' % (TARGET_HOST, PROJECT_NAME)
+        url = '%sjob/%s/buildHistory/ajax' % (TARGET_HOST, PROJECT_NAMES[self.select])
         r = requests.get(url, headers={'n': str(number[0])})
         if r.ok:
             root = etree.HTML(r.text)
@@ -43,6 +43,9 @@ class BuildProjects(object):
                 log_list = []
                 for tr in trs:
                     build_desc = tr.xpath(".//div[@class='pane desc indent-multiline']")
+                    display_name = ''
+                    details = ''
+                    build_progress = ''
                     if len(build_desc):
                         # 不为空说明，当前是构建状态
                         display_name = tr.xpath(".//div[@class='pane build-name']/a")[0].text
@@ -51,7 +54,7 @@ class BuildProjects(object):
                         tooltip = tooltip[0: tooltip.index('>')].strip()
                         if tooltip == u'In progress':
                             td = tr.xpath(".//div[@class='pane build-details']/table/tbody/tr/td")
-                            build_progress = u'（%s）' % td[0].attrib['style'].replace('width:', '').replace(';', '')
+                            build_progress = u'（Building %s）' % td[0].attrib['style'].replace('width:', '').replace(';', '')
                         elif tooltip == u'Success':
                             build_progress = u'（构建完成）'
                         elif tooltip == u'Aborted':
@@ -70,6 +73,15 @@ class BuildProjects(object):
 
                     log_list.append(u'%s %s %s' % (display_name, details, build_progress))
 
+                # 当所有任务都被取消的话，不再进行轮询
+                count = 0
+                for log in log_list:
+                    if 'Building' in log:
+                        count = count + 1
+                if count <= 0:
+                    print('全部构建完成')
+                    return None
+
                 if len(log_list):
                     log_list.reverse()
                     os.system('clear')
@@ -84,10 +96,10 @@ class BuildProjects(object):
 
     # 更新构建任务的进度并打印
     def updateAndPrintLogMsg(self, tempNumber=None):
-        job_info = self.server.get_job_info(PROJECT_NAME)
+        job_info = self.server.get_job_info(PROJECT_NAMES[self.select])
         number = job_info['lastBuild']['number']  # 最后的任务
         if tempNumber == number:
-            print 'done'
+            print('程序关闭')
             return
         # 每5秒更新一次进度信息
         self.waitfor(self.conCurrentProgress, 5, number)
@@ -98,7 +110,8 @@ class BuildProjects(object):
     def run(self):
         path = os.path.dirname(os.path.realpath(__file__)).replace('/function', '')
         workbook = xlrd.open_workbook(path + r'/file/apps.xlsx')
-        sheet_name = workbook.sheet_names()[0]
+
+        sheet_name = workbook.sheet_names()[self.select]
         sheet = workbook.sheet_by_name(sheet_name)
         rows = sheet.nrows
         for row in range(0, rows):
@@ -112,23 +125,43 @@ class BuildProjects(object):
 
         if len(self.lines):
             cols = len(self.lines[0])  # 获取列数
-            # 等待至少30秒的时间让Jenkins准备好
-            if self.server.wait_for_normal_op(30):
-                # Jenkins已经运行
-                for row in range(1, len(self.lines)):
-                    params = []
-                    for col in range(0, cols):
-                        params.append(
-                            {"name": self.lines[0][col].encode("utf8"), "value": self.lines[row][col].encode("utf8")})
+            for row in range(1, len(self.lines)):
+                params = []
+                version = ''
+                appicon = ''
+                appname = ''
+                xiaomi = ''
+                for col in range(0, cols):
+                    if self.lines[0][col] == 'ParamTag':
+                        version = "%s_" % self.lines[row][col]
+                    if self.lines[0][col] == 'ParamAppIcon':
+                        appicon = self.lines[row][col]
+                    if self.lines[0][col] == 'ParamAppName':
+                        appname = self.lines[row][col]
+                    if self.lines[0][col] == 'ParamXiaomiHidden':
+                        xiaomi = '小米登陆：%s' % self.lines[row][col]
+                    params.append({"name": self.lines[0][col], "value": self.lines[row][col]})
 
-                    self.server.build_job(PROJECT_NAME, parameters={"parameter": params})
+                name = '%s%s_%s_%s' % (version, appicon, appname, xiaomi)
+                url = '%sjob/%s/build?delay=0sec' % (TARGET_HOST, PROJECT_NAMES[self.select])
+                data = {'json': '{"parameter": %s, "statusCode": "303", "redirectTo": "."}' % str(params).replace('\'',
+                                                                                                                  '"')}  # 定义参数
+                #print(name, data)
+                res = requests.post(url, data=data)
+                if res.ok:
+                    print('构建 %s 成功' % name)
+                else:
+                    print('构建 %s 失败' % name)
 
-                #### 到此为止，所有任务均已创建成功
-                # 开始打印日志
-                self.updateAndPrintLogMsg(0)
-            else:
-                print("连接 Jenkins 失败！")
+            #### 到此为止，所有任务均已创建成功
+            # 开始打印日志
+            time.sleep(3)
+            self.updateAndPrintLogMsg(0)
 
 
 if __name__ == "__main__":
-    startBuildProjects()
+    if len(sys.argv) > 1 and sys.argv[1] in ['0', '1', '2']:
+        crawl = BuildProjects()
+        crawl.run()
+    else:
+        print('需要参数（0 构建Develop，1 构建Release，2 构建tinker）')
